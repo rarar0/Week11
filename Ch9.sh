b5626c431a52fb3354574618fac4b1bf66825ad2
rarar0/0 <boombot101@gmail.com>
2018-03-28 15:43:06 -0500

    Update Ch9.sh

--- Ch9.sh
+++ Ch9.sh
@@ -1,1 +1,20 @@
 #
+ifconfig -a
+ip
+route -n
+route add -net default gw address
+route del -net default
+ifup ifdown
+traceroute -n
+host www.google.com
+ping 
+arp -n
+nm-tool
+nmcli
+netstat -nt
+iptables -L
+dhclient eth0
+/var/state/dhclient.leases
+/etc/NetworkManager/NetworkManager.conf
+/etc/hosts
+/etc/services
