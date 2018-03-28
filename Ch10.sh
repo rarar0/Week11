c1e48c0fb0b05b1cc6f432a7de4352eede8d3e9e
rarar0/0 <boombot101@gmail.com>
2018-03-28 16:01:28 -0500

    Update Ch10.sh

--- Ch10.sh
+++ Ch10.sh
@@ -1,1 +1,24 @@
+ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key
+service sshd start
+scp user@host:filename
+scp file user@host:dir
+scp user1@host1:file user2@host2:dir
+get
+put
+lsof -i
+lsof -n -i
+lsof -i:port#
+lsof -iprotocol@host:port
+lsof -iTCP@80
+tcpdump
+tcpdump udp or port 80
+netcat -l 80
+#or nc
+nmap host
 
+#security services
+ftpd
+telnetd
+rlogind
+rexecd
+fingerd
